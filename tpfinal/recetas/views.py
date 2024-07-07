08f49be6b9a15acb88ec3f182a029734eedeec82
FerPalearuzza <ferpalearuzza1955@gmail.com>
2024-07-06 21:38:45 -0300

    funionaagit add --all recetas.html alimentada con un modelogit add --all

--- tpfinal/recetas/views.py
+++ tpfinal/recetas/views.py
@@ -4,6 +4,8 @@ from .models import Receta
 
 def recetas(request):
     recetas = Receta.objects.all()
-    return render(request, "recetas/recetas.html", {'recetas': recetas})
+    name_receta = recetas[0].name
+    primera_receta = recetas[0]
+    return render(request, "recetas/recetas.html", {'recetas': recetas, 'name_receta': name_receta, 'priera_receta':primera_receta})
     #son distintos así se con cual accedo desde el template
     
